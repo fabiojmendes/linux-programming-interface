f41bb78fe1f91507e8718eda10dfb1a4deef4e3b
Fabio Mendes <fabiojmendes@gmail.com>
2020-12-01 04:38:41 +0000

    Adding stuff from ch4

--- ch3/reboot.c
+++ ch3/reboot.c
@@ -13,5 +13,10 @@ int sum(int p1, int p2)
 int main()
 {
     printf("Hello there!\n");
+    printf("  %#x\n", LINUX_REBOOT_MAGIC1);
+    printf("  %#x\n", LINUX_REBOOT_MAGIC2);
+    printf("  %#x\n", LINUX_REBOOT_MAGIC2A);
+    printf("  %#x\n", LINUX_REBOOT_MAGIC2B);
+    printf("  %#x\n", LINUX_REBOOT_MAGIC2C);
     return sum(1, 2) - 3;
 }
---
+++ ch4/open.c
@@ -0,0 +1,34 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Pass at least one file\n",);
+    exit(EXIT_FAILURE);
+  }
+
+  int fd = open(argv[1], O_RDONLY);
+  if (fd == -1) {
+    fprintf(stderr, "Error opening file (%d)\n", errno);
+    exit(EXIT_FAILURE);
+  }
+
+  char buff[1025];
+  while (1) {
+    int n = read(fd, buff, 1024);
+    if (n == -1) {
+      fprintf(stderr, "Error reading file (%d)\n", errno);
+      exit(EXIT_FAILURE);
+    }
+    if (n == 0) break;
+
+    buff[n] = '\0';
+    printf("%s", buff);
+  }
+  close(fd);
+
+  puts("Finish");
+}
