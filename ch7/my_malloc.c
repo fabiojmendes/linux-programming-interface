76437a993f3f94e0c3f69cc6973fde96da1ea354
Fabio Mendes <fabiojmendes@gmail.com>
2020-12-10 05:26:40 +0000

    Small refactoring

--- ch7/my_malloc.c
+++ ch7/my_malloc.c
@@ -18,50 +18,47 @@ extern char end;
 
 struct node *head = NULL;
 
-void *current_brk = NULL;
-
-void *my_malloc(size_t size) {
-  if (head == NULL) {
-    current_brk = sbrk(256);
-    fprintf(stderr, "Starting heap at %p end symbol at %p\n", //
-            current_brk, &end);
-    head = (struct node *)current_brk;
-    *head = (struct node){.used = 1};
-    current_brk += 256;
+struct node *allocate(struct node *prev, size_t size) {
+  struct node *new;
+  if (prev == NULL) {
+    new = sbrk(0);
+    head = new;
+  } else {
+    new = (void *)(prev + 1) + prev->size;
+    prev->next = new;
   }
 
+  void *buff = new + 1;
+  int inc = (buff + size) - sbrk(0);
+  if (inc > 0) {
+    inc = inc > MIN_INC ? inc : MIN_INC;
+    void *res = sbrk(inc);
+    if (res == (void *)-1) {
+      perror("error allocating more memory");
+      exit(1);
+    }
+  }
+  *new = (struct node){.prev = prev, .size = size, .used = 1};
+  return new;
+}
+
+void *my_malloc(size_t size) {
   if (size % 16 != 0) {
     size += 16 - size % 16;
   }
 
-  struct node *itr = head;
-  while (itr) {
+  struct node *last = NULL;
+  for (struct node *itr = head; itr; itr = itr->next) {
     if (itr->size >= size && !itr->used) {
       itr->used = 1;
       return itr + 1;
     }
-    if (itr->next == NULL) {
-      break;
-    }
-    itr = itr->next;
+    last = itr;
   }
 
-  struct node *new = (void *)(itr + 1) + itr->size;
+  struct node *new = allocate(last, size);
   void *buff = new + 1;
-  int inc = (buff + size) - current_brk;
-  if (inc > 0) {
-    inc = inc > MIN_INC ? inc : MIN_INC;
-    current_brk = sbrk(inc);
-    if (current_brk == (void *)-1) {
-      perror("error allocating more memory");
-      exit(1);
-    }
-    current_brk += inc;
-  }
-  itr->next = new;
-  *new = (struct node){.prev = itr, .size = size, .used = 1};
-  char *mark = (char *)buff;
-  mark[size - 1] = 0xea;
+  ((char *)buff)[size - 1] = 0xea;
   return buff;
 }
 
